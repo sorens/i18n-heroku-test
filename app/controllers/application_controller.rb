b4d5db03aee08f692c1945b2de8a674f8b74bc76
Steve Orens <steve@orens.com>
2012-03-08 12:07:11 -0800

    fix application_controller set_locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 
   def set_locale
-    new_locale = params[:locale] || extract_locale_from_config || Rails.application.config.i18n.default_locale
-    Rails.application.config.i18n.locale = new_locale
+    new_locale = params[:locale] || extract_locale_from_config || Rails.application.config.i18n.default_locale || I18n.default_locale
     Rails.application.config.i18n.default_locale = new_locale
+    Rails.application.config.i18n.locale = new_locale
     Rails.logger.info "Rails.application.config.i18n is [#{Rails.application.config.i18n.locale}], default_locale is [#{Rails.application.config.i18n.default_locale}]"
-    I18n.locale = new_locale
     I18n.default_locale = new_locale
-    Rails.logger.info "I18n is [#{Rails.application.config.i18n.locale}], default_locale is [#{Rails.application.config.i18n.default_locale}]"
+    I18n.locale = new_locale
+    Rails.logger.info "I18n is [#{I18n.locale}], default_locale is [#{I18n.default_locale}]"
   end
 
   def extract_locale_from_config
