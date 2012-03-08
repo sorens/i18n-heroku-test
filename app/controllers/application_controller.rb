8dd9c92b6ca2611d6714cb5c318dcc4d1a8a9e9a
Steve Orens <steve@orens.com>
2012-03-08 10:24:43 -0800

    try brute forcing all I18n and Rails.application.config.i18n settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,17 @@ class ApplicationController < ActionController::Base
   before_filter :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || extract_locale_from_config || I18n.default_locale
-    Rails.logger.info "locale is [#{I18n.locale}], default_locale is [#{I18n.default_locale}]"
+    new_locale = params[:locale] || extract_locale_from_config || Rails.application.config.i18n.default_locale
+    Rails.application.config.i18n.locale = new_locale
+    Rails.application.config.i18n.default_locale = new_locale
+    Rails.logger.info "Rails.application.config.i18n is [#{Rails.application.config.i18n.locale}], default_locale is [#{Rails.application.config.i18n.default_locale}]"
+    I18n.locale = new_locale
+    I18n.default_locale = new_locale
+    Rails.logger.info "I18n is [#{Rails.application.config.i18n.locale}], default_locale is [#{Rails.application.config.i18n.default_locale}]"
   end
 
   def extract_locale_from_config
-    if Rails.application.config.respond_to? "LOCALE"
+    if Rails.application.config.respond_to? "locale"
       return Rails.application.config.locale
     else
       return nil
